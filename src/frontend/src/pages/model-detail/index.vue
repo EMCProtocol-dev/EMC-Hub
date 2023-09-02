032503bfff88e2597febb8412b2576c10adccfdf
jiw <jiwu_ma@qq.com>
2023-09-02 16:54:03 +0800

    ~

--- src/frontend/src/pages/model-detail/index.vue
+++ src/frontend/src/pages/model-detail/index.vue
@@ -59,7 +59,7 @@
           <NGridItem span="2 880:1">
             <div class="layout-left">
               <div class="carousel-wrap">
-                <NCarousel class="carousel" :autoplay="true" :show-arrow="covers.length > 1 ? true : false">
+                <NCarousel class="carousel" v-model:current-index="carouselIndex" :autoplay="true">
                   <template v-for="cover in covers">
                     <img class="cover" :src="cover" />
                   </template>
@@ -89,7 +89,11 @@
                 </div>
                 <div class="with">
                   <div class="with-label">Hash Code</div>
+<<<<<<< HEAD
                   <div class="with-value">{{ modelHashCode || '-' }}</div>
+=======
+                  <div class="with-value">{{ hashCodeSha256Short || '-' }}</div>
+>>>>>>> a9050e8 (~)
                 </div>
                 <div class="with">
                   <div class="with-label">Base Model</div>
@@ -127,6 +131,10 @@
         </NGrid>
       </template>
     </NCard>
+<<<<<<< HEAD
+=======
+    <ModelGallery :modelSn="modelSn" :modelHashCode="hashCodeSha256" :modelName="name" />
+>>>>>>> a9050e8 (~)
     <NModal v-model:show="nodeVisible" :mask-closable="false">
       <NCard :bordered="false" style="width: 88vw; max-width: 640px" content-style="padding-left:0;padding-right:0;">
         <template #header>
@@ -142,10 +150,25 @@
           </NButton>
         </template>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <NodeList :hash="nodeHashCode" @pressitem="onNodeClose" />
 =======
         <NodeList :hash="hashCode" @pressitem="onNodePressItem" />
 >>>>>>> f3be274 (9.1)
+=======
+        <NodeList :hash="nodeHashCode" @pressitem="onNodePressItem" @init="onNodeInit" />
+        <template #footer>
+          <NSpace justify="space-between" align="center" :wrap-item="false">
+            <NText style="font-size: 12px">Total {{ nodeList.length }} nodes</NText>
+            <NButton type="warning" size="large" strong @click="onNodeRun">
+              <template #icon>
+                <NIcon><IconRun /></NIcon>
+              </template>
+              Run
+            </NButton>
+          </NSpace>
+        </template>
+>>>>>>> a9050e8 (~)
       </NCard>
     </NModal>
   </div>
@@ -183,12 +206,38 @@ import {
 } from '@vicons/ionicons5';
 =======
 
-import { NCard, NH2, NH3, NSpace, NSpin, NTag, NCarousel, NDescriptions, NDescriptionsItem, NButton, NIcon, NModal, NGrid, NGridItem, useMessage, NPopselect } from 'naive-ui';
+import {
+  NCard,
+  NH2,
+  NH3,
+  NSpace,
+  NSpin,
+  NTag,
+  NCarousel,
+  NDescriptions,
+  NDescriptionsItem,
+  NButton,
+  NIcon,
+  NModal,
+  NGrid,
+  NGridItem,
+  NText,
+  useMessage,
+  NPopselect,
+} from 'naive-ui';
 import { useUserStore } from '@/stores/user';
 import { Http } from '@/tools/http';
 import { Utils } from '@/tools/utils';
+<<<<<<< HEAD
 import { DownloadSharp as IconDownload, CaretForwardCircleOutline as IconRun, CloseSharp as IconClose } from '@vicons/ionicons5';
 >>>>>>> f3be274 (9.1)
+=======
+import {
+  DownloadSharp as IconDownload,
+  CaretForwardCircleOutline as IconRun,
+  CloseSharp as IconClose,
+} from '@vicons/ionicons5';
+>>>>>>> a9050e8 (~)
 import NodeList from './node-list.vue';
 
 export default defineComponent({
@@ -207,7 +256,12 @@ export default defineComponent({
     NModal,
     NGrid,
     NGridItem,
+<<<<<<< HEAD
     IconDownload,
+=======
+    NText,
+    NPopselect,
+>>>>>>> a9050e8 (~)
     IconRun,
     IconClose,
     NodeList,
@@ -219,11 +273,18 @@ export default defineComponent({
     const modelId = ref(route.params.id);
     const error = ref(-1);
     const errorText = ref('');
+    const carouselIndex = ref(0);
+
     //modal property
     const nodeVisible = ref(false);
 <<<<<<< HEAD
+<<<<<<< HEAD
     const nodeHashCode = ref('');
 =======
+=======
+    const nodeHashCode = ref('');
+    const nodeList = ref<NodeItem[]>([]);
+>>>>>>> a9050e8 (~)
     const router = useRouter();
 >>>>>>> f3be274 (9.1)
 
@@ -233,7 +294,12 @@ export default defineComponent({
     const version = ref('');
     const tags = ref<string[]>([]);
     const baseModel = ref('');
+<<<<<<< HEAD
     const modelHashCode = ref('');
+=======
+    const hashCodeSha256 = ref('');
+    const hashCodeSha256Short = ref('');
+>>>>>>> a9050e8 (~)
     const floatingPoint = ref('');
     const modelSize = ref('');
     const description = ref('');
@@ -257,6 +323,7 @@ export default defineComponent({
       if (typeof data.sampleImgFileLinks !== 'string') {
         data.sampleImgFileLinks = '';
       }
+<<<<<<< HEAD
 
       if (typeof data.modelFileLinks !== 'string') {
         data.modelFileLinks = '';
@@ -272,6 +339,10 @@ export default defineComponent({
         }
       });
       let _covers: string[] = data.sampleImgFileLinks.split(',');
+=======
+      const _hashCodeSha256 = 'aa9c45d00a'; // (lastestVersion.hashCodeSha256 || '-').trim().toLocaleLowerCase();
+      const _shortHashCodeSha256 = _hashCodeSha256.substring(0, 10);
+>>>>>>> a9050e8 (~)
       name.value = data.modelName;
       covers.value = _covers;
       version.value = data.version || '1';
@@ -282,6 +353,16 @@ export default defineComponent({
       floatingPoint.value = data.floatingPoint;
       modelSize.value = data.modelSize;
       description.value = data.description;
+<<<<<<< HEAD
+=======
+      version.value = lastestVersion.modelVersion || '-';
+      hashCodeSha256.value = _hashCodeSha256;
+      hashCodeSha256Short.value = _shortHashCodeSha256;
+      baseModel.value = lastestVersion.baseModel;
+      floatingPoint.value = lastestVersion.floatingPoint;
+      modelSize.value = lastestVersion.modelSize;
+      archive.value = _modelFile.url || '';
+>>>>>>> a9050e8 (~)
       error.value = 0;
     };
 
@@ -292,6 +373,7 @@ export default defineComponent({
     return {
       error,
       errorText,
+<<<<<<< HEAD
 
       nodeVisible,
 <<<<<<< HEAD
@@ -299,17 +381,30 @@ export default defineComponent({
 
 =======
 >>>>>>> f3be274 (9.1)
+=======
+      modelSn,
+      carouselIndex,
+      nodeVisible,
+      nodeHashCode,
+      nodeList,
+>>>>>>> a9050e8 (~)
       name,
       covers,
       version,
       tags,
       archive,
       baseModel,
+<<<<<<< HEAD
       modelHashCode,
+=======
+      hashCodeSha256,
+      hashCodeSha256Short,
+>>>>>>> a9050e8 (~)
       floatingPoint,
       modelSize,
       description,
       onPressRun() {
+<<<<<<< HEAD
         if (!modelHashCode.value) {
           message.error('Sorry, This model without \'Hash Code\'');
           return;
@@ -319,12 +414,61 @@ export default defineComponent({
         nodeHashCode.value = modelHashCode.value;
 =======
 >>>>>>> f3be274 (9.1)
+=======
+        if (!hashCodeSha256Short.value || hashCodeSha256Short.value === '-') {
+          message.error("Sorry, This model without 'Hash Code'");
+          return;
+        }
+        nodeVisible.value = true;
+        nodeHashCode.value = hashCodeSha256Short.value;
+>>>>>>> a9050e8 (~)
       },
       onNodeClose() {
         nodeVisible.value = false;
       },
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+      onNodeInit(list: NodeItem[]) {
+        nodeList.value = list;
+      },
+      async onNodeRun() {
+        let sdWindow: WindowProxy | null;
+        let parameters = '';
+        let coverIndex = carouselIndex.value;
+        if (covers.value[coverIndex].parameters) {
+          parameters = covers.value[coverIndex].parameters;
+        } else {
+          try {
+            parameters = await parametersWith(covers.value[coverIndex].url);
+          } catch (e) {
+            console.error(e);
+          }
+        }
+
+        console.info('image parameters :\n', parameters);
+        
+        if (parameters) {
+          const handleMessage = (event: MessageEvent) => {
+            const request: any = event.data as any;
+            if (request.type === 'emchub-txt2img-ready') {
+              sdWindow?.postMessage({ type: 'emchub-txt2img-parameters', data: parameters }, '*');
+            }
+            window.removeEventListener('message', handleMessage);
+          };
+          window.addEventListener('message', handleMessage);
+        } else {
+          console.warn(`${covers.value[0].url} can not parse parameters`);
+        }
+
+        nodeVisible.value = false;
+        const host = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://models.emchub.ai';
+        sdWindow = window.open(`${host}/#/sd/${nodeHashCode.value}`);
+
+        // router.push({ name: 'sd', params: { modelHashCode: nodeHashCode.value } });
+      },
+>>>>>>> a9050e8 (~)
       async onNodePressItem(item: NodeItem) {
         let sdWindow: WindowProxy | null;
         let parameters = '';
@@ -334,20 +478,6 @@ export default defineComponent({
         } else {
           try {
             parameters = await parametersWith(covers.value[0].url);
-            //parameters = 'Prompt\nNavite Promp\nStep:1;Seed:1;';
-            //import {format as parameterFormat} from '@/tools/parameters';
-            //const pf = parameterFormat(parameters);
-            // {
-            //   prompt: '',
-            //   negativePrompt: '',
-            //   steps: 0,
-            //   sampler: '',
-            //   width: 0,
-            //   height: 0,
-            //   cfgScale: 0,
-            //   seed: '',
-            //   clipSkip: 0,
-            // }
           } catch (e) {
             console.error(e);
           }
@@ -368,7 +498,10 @@ export default defineComponent({
         }
 
         nodeVisible.value = false;
-        sdWindow = window.open(`https://sd.edgematrix.pro/#/txt2img?nodeid=${item.nodeId}`, `sd-window-${new Date().getTime()}`);
+        sdWindow = window.open(
+          `https://sd.edgematrix.pro/#/txt2img?nodeid=${item.nodeId}`,
+          `sd-window-${new Date().getTime()}`
+        );
         // sdWindow = window.open(
         //   `http://localhost:8080/#/txt2img?nodeid=${item.nodeId}`,
         //   `sd-window-${new Date().getTime()}`
